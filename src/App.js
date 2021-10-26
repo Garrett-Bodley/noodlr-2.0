4a6a73060b12c717e5f0686f40e6eb3e9a69d919
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-25 21:49:03 -0400

    Place Noisemaker inside a container.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-import Noisemaker from './Noisemaker/Noisemaker';
+import "./App.css";
+import Noisemaker from "./Noisemaker/Noisemaker";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  flex-grow: 1;
+  width: auto;
+  position: relative;
+`
 
 function App() {
   return (
     <div className="App">
-      <Noisemaker />
+      <Container>
+        <Noisemaker />
+      </Container>
     </div>
   );
 }
