04312064870ee158890ccff695120d4468ccf5b7
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-26 17:28:27 -0400

    Refactor animation states.

--- src/Noisemaker/Controls/PlayPauseButton.js
+++ src/Noisemaker/Controls/PlayPauseButton.js
@@ -1,35 +1,23 @@
 import React, { useCallback, useState } from "react";
 import "./playpausebutton.css";
 import PlayPauseIcon from "./Icons/PlayPauseIcon";
-import { usePlayPause } from "../../utilities/ToneProvider";
 import { animated, config, useSpring } from "react-spring";
-import styled from "styled-components";
-import { color, space, layout } from "styled-system";
 
-const StyledPlayPause = styled(PlayPauseIcon)`
-  ${color};
-  ${layout};
-  ${space};
-`;
+const parseConfig = (isPressed, isActive, isHovered) => {
+  if (isActive) return config.stiff;
+  if (isPressed || isHovered) return config.wobbly;
+  return { friction: 50, tension: 250 };
+};
 
 const PlayPauseButton = () => {
   const [isPressed, setIsPressed] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
   const [isActive, setIsActive] = useState(false);
 
-  const togglePlay = usePlayPause();
-
   const pressButton = (e) => {
     e.preventDefault();
-    togglePlay()
     setIsPressed(!isPressed);
   };
-
-  const parseConfig = useCallback(() => {
-    if (isActive) return config.stiff;
-    if (isPressed || isHovered) return config.wobbly;
-    return { friction: 50, tension: 250 };
-  }, [isHovered, isPressed, isActive]);
 
   const handleMouseDown = () => {
     setIsActive(true);
@@ -48,20 +36,20 @@ const PlayPauseButton = () => {
   };
 
   const shadowCallback = useCallback(() => {
-    if (isActive || isPressed)
-      return "translate(0px, 0px) scaleY(0.94) scaleX(1)";
-    if (isHovered) return "translate(3px, 5px) scaleY(1) scaleX(1.02)";
-    return "translate(2px, 3px) scaleY(1) scaleX(1.01)";
+    if (isActive) return "translate(0px, 0px) scaleY(0.94) scaleX(1)";
+    if (isPressed) return "translate(1px, 2px) scaleY(1) scaleX(1.01)";
+    if (isHovered) return "translate(2px, 3px) scaleY(1) scaleX(1.02)";
+    return "translate(1px, 2px) scaleY(1) scaleX(1.01)";
   }, [isPressed, isHovered, isActive]);
 
   const shadowProps = useSpring({
     from: { transform: "translate(2px, 3px) scaleX(1)" },
     to: { transform: shadowCallback() },
-    config: parseConfig()
+    config: () => parseConfig(isPressed, isActive, isHovered)
   });
 
   const edgeCallback = useCallback(() => {
-    if (isActive) return "translateY(3px) scaleY(0.88)";
+    if (isActive) return "translateY(2px) scaleY(0.925)";
     if (isPressed) return "translateY(2px) scaleY(0.925)";
     return "translateY(0px) scaleY(1)";
   }, [isPressed, isActive]);
@@ -71,14 +59,14 @@ const PlayPauseButton = () => {
     to: {
       transform: edgeCallback()
     },
-    config: parseConfig()
+    config: () => parseConfig(isPressed, isActive, isHovered)
   });
 
   const frontCallback = useCallback(() => {
-    if (isActive) return "translateY(3px) scaleY(0.88)";
-    if (isPressed) return "translateY(2px) scaleY(0.925)";
-    if (isHovered) return "translateY(-5px) scaleY(1)";
-    return "translateY(-3px) scaleY(1)";
+    if (isActive) return "translateY(2px) scaleY(0.925)";
+    if (isPressed) return "translateY(-1px) scaleY(1)";
+    if (isHovered) return "translateY(-3px) scaleY(1)";
+    return "translateY(-2px) scaleY(1)";
   }, [isHovered, isPressed, isActive]);
 
   const frontProps = useSpring({
@@ -88,24 +76,23 @@ const PlayPauseButton = () => {
     to: {
       transform: frontCallback()
     },
-    config: parseConfig()
+    config: () => parseConfig(isPressed, isActive, isHovered)
   });
 
   const innerShadowCallback = useCallback(() => {
     if (isActive) return { opacity: "70%" };
-    if (isPressed) return { opacity: "60%" };
     return { opacity: "0%" };
-  }, [isPressed, isActive]);
+  }, [isActive]);
 
   const innerShadowProps = useSpring({
     from: { opacity: "0%" },
     to: innerShadowCallback(),
-    config: parseConfig()
+    config: { ...() => parseConfig(isPressed, isActive, isHovered), delay: 300 }
   });
 
   const iconCallback = useCallback(() => {
-    if (isActive) return "translateY(5px)";
-    if (isPressed) return "translateY(4px)";
+    if (isActive) return "translateY(2px)";
+    if (isPressed) return "translateY(-1px)";
     if (isHovered) return "translateY(-5px)";
     return "translateY(-3px)";
   }, [isHovered, isPressed, isActive]);
@@ -113,13 +100,14 @@ const PlayPauseButton = () => {
   const iconProps = useSpring({
     from: { transform: "translateY(-3px" },
     to: { transform: iconCallback() },
-    config: parseConfig()
+    config: () => parseConfig(isPressed, isActive, isHovered)
   });
 
-  const buttonIconProps = useSpring({
-    from: {color: "grey"},
-    to: {color: isPressed ? "#38CC77" : "grey"},
-  })
+  const svgProps = useSpring({
+    from: { fill: "grey" },
+    to: { fill: isPressed ? "greenyellow" : "grey" },
+    config: config.stiff
+  });
 
   return (
     <label
@@ -146,7 +134,9 @@ const PlayPauseButton = () => {
         ></animated.span>
       </animated.span>
       <animated.div style={iconProps} className="playpause-icon">
-        <StyledPlayPause style={buttonIconProps} />
+        <PlayPauseIcon
+          style={svgProps}
+        />
       </animated.div>
     </label>
   );
