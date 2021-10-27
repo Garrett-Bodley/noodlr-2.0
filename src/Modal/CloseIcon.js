276708608df8823a836d780be5050ee4d205074d
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-27 17:31:48 -0400

    Make CloseIcon component.

---
+++ src/Modal/CloseIcon.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const CloseIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 512 512"
+      xmlSpace="preserve"
+    >
+      <g>
+        <g>
+          <polygon points="512,59.076 452.922,0 256,196.922 59.076,0 0,59.076 196.922,256 0,452.922 59.076,512 256,315.076 452.922,512     512,452.922 315.076,256   " />
+        </g>
+      </g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+      <g></g>
+    </svg>
+  );
+};
+
+export default CloseIcon;
