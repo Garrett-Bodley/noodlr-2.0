0580293f2c85e7c8ec544115d6c9138c1caf1916
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-28 16:41:51 -0400

    Create 3d Button to show Modal.

---
+++ src/Modal/ShowModal.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from "styled-components"
+import QuestionMarkIcon from './QuestionMarkIcon'
+
+const StyledIcon = styled(QuestionMarkIcon)`
+  padding: 3px;
+  transform: translate(-1px, -1px);
+  height: inherit;
+  width: inherit;
+  color: var(--color-light);
+`
+
+const PositionWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  outline-offset: 5px;
+  isolation: isolate;
+  z-index: 3;
+`
+
+const Edge = styled.div`
+  background-color: black;
+  height: 3em;
+  width: 3em;
+  margin: 2em;
+  border-radius: 50%;
+  position: absolute;
+  box-shadow:
+  0.3px 0.3px 0.3px rgba(0, 0, 0, 0.028),
+  0.7px 0.7px 0.7px rgba(0, 0, 0, 0.04),
+  1.3px 1.3px 1.3px rgba(0, 0, 0, 0.05),
+  2.2px 2.2px 2.2px rgba(0, 0, 0, 0.06),
+  4.2px 4.2px 4.2px rgba(0, 0, 0, 0.072),
+  10px 10px 10px rgba(0, 0, 0, 0.1)
+;
+
+`
+
+const Front = styled.div`
+  cursor: pointer;
+  background-color: var(--color-dark);
+  border: 1px solid var(--color-dark--dark);
+  height: 3em;
+  width: 3em;
+  margin: 2em;
+  border-radius: 50%;
+  position: relative;
+  transform: translateY(-5px);
+  z-index: 5000;
+
+  &:active {
+    filter: brightness(1.1)
+  }
+`
+
+const ShowModal = ({ showModal }) => {
+
+  return (
+    <PositionWrapper>
+      <Edge/>
+      <Front onClick={showModal}>
+        <StyledIcon/>
+      </Front>
+    </PositionWrapper>
+  )
+}
+
+export default ShowModal
