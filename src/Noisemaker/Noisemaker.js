fe97def354cbf258a2a36b691d67879f6b99c1ba
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-29 13:04:09 -0400

    Refactor to remove unnecessary style sheet. Add border and make code prettier.

--- src/Noisemaker/Noisemaker.js
+++ src/Noisemaker/Noisemaker.js
@@ -1,12 +1,21 @@
 import React from "react";
-import styled from 'styled-components';
-import "./noisemaker.css";
-import { border, color, flexbox, grid, layout, space } from 'styled-system';
-import Grid from './Grid/Grid'
-import Controls from './Controls/Controls'
+import styled from "styled-components";
+import {
+  border,
+  color,
+  flexbox,
+  grid,
+  layout,
+  position,
+  shadow,
+  space,
+} from "styled-system";
+
+import Controls from "./Controls/Controls";
+import Grid from "./Grid/Grid";
+import ToneProvider from "../utilities/ToneProvider";
 import VampProvider from "../utilities/VampProvider";
 
-import ToneProvider from "../utilities/ToneProvider";
 
 const Container = styled.section`
   ${border}
@@ -14,23 +23,84 @@ const Container = styled.section`
   ${flexbox}
   ${grid}
   ${layout}
+  ${position}
+  ${shadow}
   ${space}
-`
+
+  &::before {
+    content: "";
+    z-index: -1;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background: rgb(52, 53, 49);
+    background: -moz-linear-gradient(
+      90deg,
+      rgba(52, 53, 49, 1) 0%,
+      rgba(34, 35, 31, 1) 5%,
+      rgba(34, 35, 31, 1) 98%,
+      rgba(52, 53, 49, 1) 100%
+    );
+    background: -webkit-linear-gradient(
+      90deg,
+      rgba(52, 53, 49, 1) 0%,
+      rgba(34, 35, 31, 1) 5%,
+      rgba(34, 35, 31, 1) 98%,
+      rgba(52, 53, 49, 1) 100%
+    );
+    background: linear-gradient(
+      90deg,
+      rgba(52, 53, 49, 1) 0%,
+      rgba(34, 35, 31, 1) 5%,
+      rgba(34, 35, 31, 1) 98%,
+      rgba(52, 53, 49, 1) 100%
+    );
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#343531",endColorstr="#343531",GradientType=1);
+    transform: translateY(10px);
+    border: 1px solid var(--color-dark--dark);
+    border-radius: 12px;
+    box-shadow: 0.6px 0.6px 0.6px rgba(0, 0, 0, 0.028),
+      1.3px 1.3px 1.3px rgba(0, 0, 0, 0.04),
+      2.5px 2.5px 2.5px rgba(0, 0, 0, 0.05),
+      4.5px 4.5px 4.5px rgba(0, 0, 0, 0.06),
+      8.4px 8.4px 8.4px rgba(0, 0, 0, 0.072), 20px 20px 20px rgba(0, 0, 0, 0.1);
+  }
+`;
 
 const StyledGrid = styled(Grid)`
   ${grid}
   ${layout}
   ${space}
-`
+`;
 
 const Noisemaker = (props) => {
-
   return (
     <VampProvider>
       <ToneProvider>
-        <Container className="noisemaker" {...props} bg="#3b3c36" justifyContent="center" mx="1em" borderRadius="12px" display="grid" gridTemplateColumns="4fr 2fr" gridTemplateAreas={' "tones controls" '}>
-            <StyledGrid gridArea="tones" my="auto" rowCount={9} beatCount={16} p=".5em 1em 1em 1em" colors={{primary: '#38CC77', secondary: '#DE4839'}} />
-          <Controls gridArea="controls"/>
+        <Container
+          className="noisemaker"
+          {...props}
+          bg="#3b3c36"
+          boxShadow="inset 0px 0px 0px 1px var(--color-dark--dark);"
+          justifyContent="center"
+          mx="1em"
+          position="relative"
+          borderRadius="12px"
+          display="grid"
+          gridTemplateColumns="4fr 2fr"
+          gridTemplateAreas={' "tones controls" '}
+        >
+          <StyledGrid
+            gridArea="tones"
+            my="auto"
+            rowCount={9}
+            beatCount={16}
+            p=".5em 1em 1em 1em"
+            colors={{ primary: "#38CC77", secondary: "#DE4839" }}
+          />
+          <Controls gridArea="controls" />
         </Container>
       </ToneProvider>
     </VampProvider>
