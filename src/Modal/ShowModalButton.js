cf4a85ad006f0f4b2c6bc3c1154f786b51482bc8
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-30 23:51:03 -0400

    Add boop effect to button. Reorganize code.

--- src/Modal/ShowModalButton.js
+++ src/Modal/ShowModalButton.js
@@ -1,24 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { config, useSpring } from "react-spring";
 import styled from "styled-components";
-import QuestionMarkIcon from "./QuestionMarkIcon";
 import { useModalVisUpdate } from "../utilities/ModalVisProvider";
-
-const StyledIcon = styled(QuestionMarkIcon)`
-  padding: 3px;
-  transform: translate(-1px, -1px);
-  height: inherit;
-  width: inherit;
-  color: var(--color-light);
-`;
-
-const PositionWrapper = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-  outline-offset: 5px;
-  isolation: isolate;
-  z-index: 3;
-`;
+import QuestionMarkIcon from "./QuestionMarkIcon";
 
 const Edge = styled.div`
   background-color: black;
@@ -29,10 +13,10 @@ const Edge = styled.div`
   position: absolute;
   box-shadow: 
     0.3px 0.3px 0.3px rgba(0, 0, 0, 0.028),
-    0.7px 0.7px 0.7px rgba(0, 0, 0, 0.04),
+    0.7px 0.7px 0.7px rgba(0, 0, 0, 0.04), 
     1.3px 1.3px 1.3px rgba(0, 0, 0, 0.05),
     2.2px 2.2px 2.2px rgba(0, 0, 0, 0.06),
-    4.2px 4.2px 4.2px rgba(0, 0, 0, 0.072),
+    4.2px 4.2px 4.2px rgba(0, 0, 0, 0.072), 
     10px 10px 10px rgba(0, 0, 0, 0.1);
 `;
 
@@ -47,14 +31,59 @@ const Front = styled.div`
   position: relative;
   transform: translateY(-5px);
   z-index: 5000;
+  will-change: filter;
+  transition-delay: 20ms;
 
   &:hover {
     filter: brightness(1.1);
   }
 `;
 
+const PositionWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  outline-offset: 5px;
+  isolation: isolate;
+  z-index: 3;
+`;
+
+const StyledIcon = styled(QuestionMarkIcon)`
+  padding: 3px;
+  transform: translate(-1px, -1px);
+  height: inherit;
+  width: inherit;
+  color: var(--color-light);
+`;
+
 const ShowModalButton = () => {
   const setIsVisible = useModalVisUpdate();
+  const [isBooped, setIsBooped] = useState(false);
+  const rotation = 30
+  const timing = 150;
+
+  const trigger = () => {
+    setIsBooped(true)
+  }
+
+  useEffect(() => {
+    if (!isBooped) return;
+
+    const timeoutId = window.setTimeout(() => {
+      setIsBooped(false);
+    }, timing);
+
+    return () => window.clearTimeout(timeoutId);
+  }, [isBooped, timing]);
+
+  const style = useSpring({
+    transform: isBooped 
+      ? `translate(-1px, -1px)
+        rotate(${rotation}deg)` 
+      : `translate(-1px, -1px)
+        rotate(0deg)`,
+    config: config.wobbly
+  });
 
   const showModal = () => {
     setIsVisible(true);
@@ -63,8 +92,8 @@ const ShowModalButton = () => {
   return (
     <PositionWrapper>
       <Edge />
-      <Front onClick={showModal}>
-        <StyledIcon />
+      <Front onClick={showModal} onMouseEnter={trigger}>
+        <StyledIcon style={style} />
       </Front>
     </PositionWrapper>
   );
