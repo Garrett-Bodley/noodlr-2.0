6a643ff0d74c643ff6fdd836d9b4057324832b1c
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-22 17:53:17 -0500

    Create context provider for mousedown event.

---
+++ src/utilities/MouseDownProvider.js
@@ -0,0 +1,32 @@
+import React, {useContext, useState} from 'react'
+
+const MouseDownContext = React.createContext()
+
+export const useMouseDown = () => {
+  return useContext(MouseDownContext)
+}
+
+const MouseDownProvider = ({children}) => {
+
+  const [mouseDown, setMouseDown] = useState(false)
+
+  const handleMouseDown = e => {
+    e.preventDefault()
+    setMouseDown(true)
+  }
+
+  const handleMouseUp = e => {
+    e.preventDefault()
+    setMouseDown(false)
+  }
+
+  return (
+    <MouseDownContext.Provider value={mouseDown}>
+      <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}>
+        {children}
+      </div>
+    </MouseDownContext.Provider>
+  )
+}
+
+export default MouseDownProvider
