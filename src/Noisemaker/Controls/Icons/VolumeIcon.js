64a87fa62a0203d92125a696dea2b8d3c0576b65
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-22 18:20:06 -0500

    Change icons and icon-styling.

--- src/Noisemaker/Controls/Icons/VolumeIcon.js
+++ src/Noisemaker/Controls/Icons/VolumeIcon.js
@@ -1,20 +1,26 @@
 const VolumeIcon = (props) => {
   return (
-    <svg
-      {...props}
-      viewBox="0 0 24 24"
-      height="48"
-      width="48"
-      focusable="false"
-      role="img"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
-      <path d="M19.07 4.93a10 10 0 010 14.14M15.54 8.46a5 5 0 010 7.07"></path>
+    <svg {...props} viewBox="10 10 80 80" xmlns="http://www.w3.org/2000/svg">
+      <path d="M63.17,26.42a3,3,0,0,0-3.28.47L48.25,37.58l-13.41.69a4.14,4.14,0,0,0-4.1,4.14v.81c-2.85.15-5.92,1.11-5.92,7.39s3.07,7.25,5.92,7.4v.92a4.16,4.16,0,0,0,4.1,4.15l13.46.69,11.62,9.36a3.06,3.06,0,0,0,5-2.33V29.2A3,3,0,0,0,63.17,26.42ZM28.91,54.55c-.73-.5-1.09-1.83-1.09-3.94s.36-3.43,1.09-3.93a3.73,3.73,0,0,1,1.83-.46V55A3.73,3.73,0,0,1,28.91,54.55Zm6,5.53a1.15,1.15,0,0,1-1.14-1.15V42.41a1.14,1.14,0,0,1,1.14-1.14l11.47-.59v20C46.27,60.66,35,60.07,34.88,60.08ZM62,70.8s0,0,0,0-.07,0-.1,0l-12-9.68a1.72,1.72,0,0,0-.46-.25V40.46a1.57,1.57,0,0,0,.53-.31l12-11a0,0,0,0,1,.05,0C62,29.16,61.92,70.74,62,70.8Z" />
+      <path d="m69 40.19a1.49 1.49 0 0 0 0 2.12 10.89 10.89 0 0 1 0 15.38 1.5 1.5 0 0 0 2.12 2.12 13.89 13.89 0 0 0 0-19.62 1.49 1.49 0 0 0-2.12 0z" />
+      {/* <text
+        y="115"
+        fill="#000000"
+        font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+        font-size="5px"
+        font-weight="bold"
+      >
+        Created by Fran Couto
+      </text>
+      <text
+        y="120"
+        fill="#000000"
+        font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+        font-size="5px"
+        font-weight="bold"
+      >
+        from the Noun Project
+      </text> */}
     </svg>
   );
 };
