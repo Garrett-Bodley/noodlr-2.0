34d09659344a842654512bb7a90cbd34e93887e0
Garrett-Bodley <garrett.bodley@gmail.com>
2022-03-17 16:11:05 -0400

    Update hook filestructure. Make code prettier.

--- src/utilities/MouseDownProvider.js
+++ src/utilities/MouseDownProvider.js
@@ -1,32 +1,35 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useState } from "react";
 
-const MouseDownContext = React.createContext()
+const MouseDownContext = React.createContext();
 
 export const useMouseDown = () => {
-  return useContext(MouseDownContext)
-}
+  return useContext(MouseDownContext);
+};
 
-const MouseDownProvider = ({children}) => {
+const MouseDownProvider = ({ children }) => {
+  const [mouseDown, setMouseDown] = useState(false);
 
-  const [mouseDown, setMouseDown] = useState(false)
+  const handleMouseDown = (e) => {
+    e.preventDefault();
+    setMouseDown(true);
+  };
 
-  const handleMouseDown = e => {
-    e.preventDefault()
-    setMouseDown(true)
-  }
-
-  const handleMouseUp = e => {
-    e.preventDefault()
-    setMouseDown(false)
-  }
+  const handleMouseUp = (e) => {
+    e.preventDefault();
+    setMouseDown(false);
+  };
 
   return (
     <MouseDownContext.Provider value={mouseDown}>
-      <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}>
+      <div
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
+      >
         {children}
       </div>
     </MouseDownContext.Provider>
-  )
-}
+  );
+};
 
-export default MouseDownProvider
+export default MouseDownProvider;
